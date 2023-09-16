e167e6c1fc16b4042441d3b3ab54d8856e84a43f
muradkadir <world-s@yandex.ru>
2023-09-17 02:33:48 +0300

    Add new fun

---
+++ Demo.py
@@ -0,0 +1,6 @@
+def some_fun(a, b):
+    return a + b
+# Context from Function C:/Users/Moonwolf/PycharmProjects/qa7_demo/main.py:print_hi
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint
